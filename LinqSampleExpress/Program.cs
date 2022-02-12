5747007590c440719190c94d2df11d96053136a0
Jeff Workman <jworkhollr@aol.com>
2022-02-12 15:54:37 -0500

    Add project files.

---
+++ LinqSampleExpress/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqSampleExpress {
+
+    public class Program {
+
+        static void Main(string[] args) {
+
+           int[] scores = new int[] { 100, 97, 54, 88, 96 };
+
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            foreach (int i in scoreQuery) {
+                Console.WriteLine( i + " ");
+            }           
+        
+     
+
+        }
+    }
+}
